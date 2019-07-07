8b54506224716c642448c9d6049bf5dabe43febc
Adam Mahood <mahood.adam@gmail.com>
2019-07-06 22:10:42 -0400

    [lint] add more lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,22 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    es6: true
   },
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended'
   ],
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018
+  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'comma-dangle': ['error', 'never'],
+    'brace-style': '1tbs',
+    'indent': ['error', 'tab', { SwitchCase: 1 }]
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
 }
